15f73e0ec5c06999c721d95dd74c1b16acf210e2
D4rkAng3l-Fnatic <duarte.ferreira0802@gmail.com>
2021-05-19 16:41:33 +0100

    navbar

--- Components/card.php
+++ Components/card.php
@@ -1,47 +1,92 @@
-<div class="cartatodo">
-    <h3 id="texto2"><strong>3. O que fazemos</strong></h3>
-    <div class="container carta">
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                <img src="./images/sketch (1).png" alt="">
-                <h3>Branding</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <a href="#">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                <img src="./images/data.png" alt="">
-                <h3>Web Design</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <a href="#">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="face face1">
-                <div class="content">
-                <img src="./images/advertising.png" alt="">
-                <h3>Social Media</h3>
-                </div>
-            </div>
-            <div class="face face2">
-                <div class="content">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <a href="#">Read More</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<h3 id="texto2"><strong>3. O que fazemos</strong></h3>
+<div class="row linhacarta">
+      <div class="cartad">
+         <div class="cartah">
+            <img src="./images/sketch (1).png" class="imagemcard" alt="">
+            <h3>Branding</h3>
+         </div>
+         <div class="cartab">
+            <p>
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+         </div>
+      </div>
+      <div class="cartad">
+         <div class="cartah">
+            <img src="./images/data.png" class="imagemcard" alt="">
+            <h3>Web Design</h3>
+         </div>
+         <div class="cartab">
+            <p>
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+         </div>
+      </div>
+      <div class="cartad">
+         <div class="cartah">
+            <img src="./images/advertising.png" class="imagemcard" alt="">
+            <h3>Social Media</h3>
+         </div>
+         <div class="cartab">
+            <p>
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+         </div>
+      </div>
 </div>
+<style>
+.linhacarta{
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  flex-flow: wrap;
+}
+.cartad{
+  width: 20%;
+  background: white;
+  border: 1px solid #ccc;
+  margin-bottom: 5%;
+  margin-top: 5%;
+  transition: 0.3s;
+  opacity: 0.3;
+  transition: 0.3s;
+}
+.cartah{
+  text-align: center;
+  padding: 30px;
+  background: linear-gradient(to right, #ff416c, crimson);
+  color: #fff;
+}
+.cartad:hover, .cartah:hover{
+    opacity: 1;
+}
+.cartab{
+  padding: 30px 20px;
+  text-align: center;
+  font-size: 18px;
+
+}
+.cartad:hover{
+  transform: scale(1.05);
+  box-shadow: 0 0 40px -10px rgba(0, 0, 0, 0.5);
+}
+.imagemcard{
+    width: 40%;
+    padding-bottom: 20px;
+}
+@media screen and (max-width:1000px){
+   .cartad{
+      width: 40%;
+   }
+}
+@media screen and (max-width:700px){
+   .cartad{
+      width: 60%;
+   }
+}
+@media screen and (max-width:500px){
+   .cartad{
+      width: 60%;
+   }
+}
+</style>
--- HeaderFooter/Nav.php
+++ HeaderFooter/Nav.php
@@ -1,18 +1,29 @@
 <header>
-  <div class="container navegar">
-      <nav>
+  <div class="navegar">
+  <nav>
         <div class="hamburger">
           <div class="line"></div>
           <div class="line"></div>
           <div class="line"></div>
         </div>
         <ul class="nav-links">
-          <img class="logo1" src="Images/novo1.png" fluid alt="logo">
-          <li><a href="?pagina=Home">Home</a></li>
-          <li><a href="?pagina=Sobre-Nos">Sobre Nós</a></li>
-          <li><a href="?pagina=Projetos">Projetos</a></li>
-          <li><a href="?pagina=Contactos">Contactos</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre Nós</a></li> 
+          <li><a href="#">Projetos</a></li>
+          <li><a href="#">Contactos</a></li>
+          <li><a href="#">Contactos</a></li>
         </ul>
-      </nav>
+    </nav>1
   </div>
+<script>
+   const hamburger = document.querySelector('.hamburger');
+   const navLinks = document.querySelector('.nav-links');
+   const links = document.querySelectorAll('.nav-links li');
+   hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle("open");
+      links.forEach(link => {
+         link.classList.toggle("fade");
+      });
+   });   
+</script>
 </header>
